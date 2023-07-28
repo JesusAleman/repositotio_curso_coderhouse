0593f60c3263fc63859df035218d934b8d93b804
Jesus_Aleman <chaleman98@gmail.com>
2023-07-27 18:31:49 -0600

    /clase17_práctica/

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -6,6 +6,9 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField()
 
+    def __str__(self):
+            return f"{self.nombre} ({self.comision})"
+
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
@@ -16,6 +19,9 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
     profesion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=50)
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'America/Mexico/Monterrey'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
